f0ded564b6bd09d138a526c730f0a731fb3880e5
mattiamaffo <126592486+mattiamaffo@users.noreply.github.com>
2025-01-20 16:40:33 +0100

    Add files via upload

--- app/src/main/java/com/example/shakeit/MainActivity.kt
+++ app/src/main/java/com/example/shakeit/MainActivity.kt
@@ -1,15 +1,22 @@
 package com.example.shakeit
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.navigation.compose.rememberNavController
 import com.example.shakeit.ui.navigation.AppNavigation
 import com.example.shakeit.ui.theme.ShakeItTheme
+import org.opencv.android.OpenCVLoader
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        if (!OpenCVLoader.initDebug()) {
+            Toast.makeText(this, "OpenCV initialization failed!", Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(this, "OpenCV initialized successfully", Toast.LENGTH_SHORT).show()
+        }
         setContent {
             ShakeItTheme {
                 val navController = rememberNavController()
